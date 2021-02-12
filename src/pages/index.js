bd0657db0a5fd0fb066ef3cf8093f3ef0fc9218b
Ruan <devd8727@gmail.com>
2021-02-12 12:46:09 -0300

    Projeto exemplo fazendo uso do ssr com o next js

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Button from './components/button'
+
+
+function HomePage() {
+    return <div>
+        Welcome to Next.js!
+        <br/>
+        <Button />
+    </div>
+  }
+  
+  export default HomePage
+
+  //AQUI TEMOS 2 COMPONENTES RODANDO EM SSR, BASTA DESLIGAR O JS DA PAGINA E VERIFICAR QUE ELES CONTINUAM RODANDO
